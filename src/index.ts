5db5952f97c65187c2b69934362d3998bb64e1d3
nando-carvalho <fernando@elosistemas.inf.br>
2022-02-11 16:18:44 -0300

    Implementação da rota post na aplicação e leitores de json no express

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import express,  {Request, Response, NextFunction} from 'express'; //gerenciador
 import usersRoute from './routes/users.route';
 const app = express();
 
+//Config da aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended:true})); //serve para entender querystring entre outras coisas
+
+//Routes
 app.use(usersRoute);
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({foo: 'bar'});
 });
 
-
+//Inicialização
 app.listen(3000, () => {
     console.log('Api executando na porta 3000');
 });
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -12,6 +12,12 @@ usersRoute.get('/users/:uuid', (req: Request<{uuid:string}>, res: Response, next
     const uuid = req.params.uuid;
     // bancoDeDados.getUserByUUid(uuid);
     res.status(StatusCodes.OK).send({uuid});
-});    
+});  
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {    
+    const newUser = req.body;
+    console.log(req.body);
+    res.status(StatusCodes.CREATED).send(newUser);
+});
 
 export default usersRoute;
